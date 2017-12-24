0252e45b5e32b6f3513b80917e46e076b983ab38
kouichimurakami <murakami1119@gmail.com>
2017-12-24 18:03:35 +0900

    ファイル読込と正規表現による時刻歴の区別まで

---
+++ ccim_auto.py
@@ -0,0 +1,11 @@
+import re
+#pattern1 は、 例題　「2647, TYPE NO.=   53, POINT NO.=    10」 の2647の判定用の正規表現
+pattern1 = re.compile(r"TYPE NO.=")
+with open('Ag001hist0.txt','r',encoding='CP932') as f:
+ whole_file =f.readlines()
+ for word in whole_file:
+        if re.search(pattern1,word) == None:
+            pass
+        else:
+            print(word,end='')
+
